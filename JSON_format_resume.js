e4ad90901c80f83834517b077b0b3da756394633
YOKESHDHANABALAN <107738665+YOKESHDHANABALAN@users.noreply.github.com>
2022-06-25 17:01:35 +0530

    Add files via upload

---
+++ JSON_format_resume.js
@@ -0,0 +1,14 @@
+//create your own resume in JSON format
+let resume = 
+    {
+        "Name            ": "Yokesh.D",
+        "Address         ": "xxx street, yy city",
+        "Education       ": "B.E.CSE",
+        "Skills          ": "C, C++, Java, JavaScript",
+        "Project         ": "An efficient iot based smart Power control and monitoring system.",
+        "Language        ": "Tamil, English",
+        "Certification   ": "C programming on GUVI",
+        "Internship      ": "xxx internship in zzz company",
+        "Industrial_Visit": "hhh company"
+    }
+console.log(resume);
